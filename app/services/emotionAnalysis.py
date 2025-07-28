42db6c5115c818128c8f0895d887dd074afa1103
Chi Ngo <chingo_2027@depauw.edu>
2025-07-27 23:41:21 -0400

    feat(emotion): implement base emotion analysis logic

--- app/services/emotionAnalysis.py
+++ app/services/emotionAnalysis.py
@@ -1,1 +1,14 @@
- 
+from transformers import pipeline
+
+classifier = pipeline("text-classification", model= "j-hartmann/emotion-english-distilroberta-base", top_k=None)
+
+def analyzeEmotion(text):
+    scores = classifier(text)[0]
+    emotionScores = {score['label']:round(score['score'], 2) for score in scores}
+    dominantEmotion = max(emotionScores, key=emotionScores.get)
+    return {
+        "dominant_emotion": dominantEmotion,
+        "emotion_scores": emotionScores
+    }
+    
+                    
